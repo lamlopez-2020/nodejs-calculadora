c7ca7f56caee12d3ea4fabd7661c74afbc1675db
Luis Monagas <61388255+lamlopez-2020@users.noreply.github.com>
2023-03-06 20:28:22 -0400

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+/** @format */
+
+module.exports = {
+    env: {
+        es2021: true,
+        node: true,
+    },
+    extends: ['eslint:recommended', 'prettier'],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {
+        quotes: ['error', 'single'],
+        semi: ['error', 'never'],
+    },
+}
