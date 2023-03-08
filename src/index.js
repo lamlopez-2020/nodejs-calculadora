eea6c5ad7f29bac86e6183833def1fd0f1ce11b4
Luis Monagas <monagasweb2020@gmail.com>
2023-03-07 23:14:57 -0400

    Validar la entrada y separar las partes de la misma en operandos y operador

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 /** @format */
 
+import { INVALID_INPUT } from '#Constants/messages';
+import { OPERATORS } from '#Constants/operators';
 import { promptQuestion } from '#Lib/promptQuestion';
 
 (async () => {
@@ -8,6 +10,35 @@ import { promptQuestion } from '#Lib/promptQuestion';
     console.log(userAnswer);
 
     //2ª Validar la entrada y separar las partes de la misma en operandos y operador.
+    //Eliminar espacios al principio y final de la entrada del userAnswer
+    const standarizeInput = userAnswer.trim();
+
+    //ComprObar que no sea un string vacio
+    if (standarizeInput === '') {
+        console.log(INVALID_INPUT);
+        return;
+    }
+
+    let operator;
+
+    for (const allowedOperator of OPERATORS) {
+        if (standarizeInput.includes(allowedOperator)) {
+            if (
+                operator ||
+                standarizeInput.indexOf(allowedOperator) !==
+                    standarizeInput.lastIndexOf(allowedOperator)
+            ) {
+                console.log(INVALID_INPUT);
+                return;
+            }
+            operator = allowedOperator;
+        }
+    }
+
+    if (!operator) {
+        console.log(INVALID_INPUT);
+        return;
+    }
 
     //3ª Realizar la operación.
 
